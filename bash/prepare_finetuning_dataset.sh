f234157a4fe545d445afa7bb07876828cd19f2e1
yaldashbz <yaldashabanzadeh@gmail.com>
2024-08-20 15:42:55 +0200

    fix: some minor updates

--- bash/prepare_finetuning_dataset.sh
+++ bash/prepare_finetuning_dataset.sh
@@ -1,15 +1,20 @@
-TARGET_MODEL=TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T  # used for tokenization
-# TARGET_MODEL=facebook/opt-125m
-SEQLEN=2048
+TARGET_MODEL=meta-llama/Meta-Llama-3.1-8B-Instruct  # used for tokenization
+SEQLEN=8192
+# TARGET_MODEL=TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T  # used for tokenization
+# SEQLEN=2048
 DATASET=togethercomputer/RedPajama-Data-1T-Sample
-# DATASET_CONFIG=wikitext-2-raw-v1
-# DATASET=wikitext
-OUTPUT_PATH=../PV-GPTQ/pajama_tokenized_tinyllama-v2
+OUTPUT_PATH=/nfs/scistore19/alistgrp/yshabanz/data/pajama_tokenized_llama3.1-8b-instruct_$SEQLEN
+CACHE_DIR=/nfs/scistore19/alistgrp/yshabanz/cache_dir
 
 export HUGGINGFACE_TOKEN='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'
 
 # --dtype -> --load_dtype
 
-CUDA_VISIBLE_DEVICES=0 HF_HOME=../PV-GPTQ/hf OMP_NUM_THREADS=16 torchrun --master-port 3456 --nproc-per-node=1 finetune_fsdp.py --dataset_config_name=$DATASET_CONFIG --base_model $TARGET_MODEL --quantized_model ./doesnt_matter --load_dtype bfloat16 --block_type LlamaDecoderLayer --dataset_name=$DATASET --split train --cache_dir=./cache_dir --trust_remote_code --model_seqlen=$SEQLEN --preprocessing_num_workers=64 --preprocessing_chunk_length 100000 --save_dataset_and_exit $OUTPUT_PATH
+CUDA_VISIBLE_DEVICES=1 HF_HOME='../../hf' OMP_NUM_THREADS=16 torchrun --master-port 3456 --nproc-per-node=1 finetune_fsdp.py \
+    --dataset_config_name=$SUBSET --base_model $TARGET_MODEL --quantized_model ./doesnt_matter \
+    --load_dtype bfloat16 --block_type LlamaDecoderLayer --dataset_name=$DATASET --split train \
+    --cache_dir=$CACHE_DIR --trust_remote_code --model_seqlen=$SEQLEN --preprocessing_num_workers=64 \
+    --save_dataset_and_exit $OUTPUT_PATH \
+    # --preprocessing_chunk_length 100000
 
-tar -cvf tokenized_data_qwen2.tar $OUTPUT_PATH   # optionally pack for distribution
+# tar -cvf tokenized_data_tinyllama.tar $OUTPUT_PATH   # optionally pack for distribution
--- bash/save_best_model.sh
+++ bash/save_best_model.sh
@@ -1,20 +1,10 @@
 # Llama-3-8B
-export NAME=Meta-Llama-3-8B-Instruct-gptq4-128-True-seed0_cosmopedia_static
-export MODEL_PATH=/nfs/scistore19/alistgrp/huggingface/hub/Meta-Llama-3-8B-Instruct  # path or huggingface id of the base model
+export NAME='Meta-Llama-3.1-8B-Instruct-gptq4-128-True-seed1_mse_static'
+export SAVED_NAME=$NAME\_closed_form_3e-1
+export MODEL_PATH=meta-llama/Meta-Llama-3.1-8B-Instruct  # path or huggingface id of the base model
 export QUANTIZED_MODEL_PATH=/nfs/scistore19/alistgrp/amoeini/saved/$NAME # path to the model created by initial calibration
-export SNAPSHOT_PATH=/nfs/scistore19/alistgrp/yshabanz/saved/pv_model_llama_cosmopedia_static
-export CONVERTED_CHECKPOINT_PATH=/nfs/scistore19/alistgrp/yshabanz/saved/$NAME\_p-step
-
-# export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
-
-# TinyLlama
-# export MODEL_PATH=TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T  # path or huggingface id of the base model
-# export QUANTIZED_MODEL_PATH=/nfs/scistore19/alistgrp/yshabanz/codes/Tiny-llama-4bit-v2 # path to the model created by initial calibration
-# export TOKENIZED_DATASET_PATH=/nfs/scistore19/alistgrp/yshabanz/codes/pajama_tokenized_tinyllama-v2  # yet again, red pajama adviced
-# export CACHE_DIR=/nfs/scistore19/alistgrp/yshabanz/cache_dir
-# export SNAPSHOT_PATH=/nfs/scistore19/alistgrp/yshabanz/saved/pv_model_tinyllama_test
-# export SEQLEN=2048
-# export NUM_GPUS=4
+export SNAPSHOT_PATH=/nfs/scistore19/alistgrp/yshabanz/saved/$SAVED_NAME
+export CONVERTED_CHECKPOINT_PATH=/nfs/scistore19/alistgrp/yshabanz/saved/$SAVED_NAME\_converted
 
 
 python convert_fsdp_model_format.py\
